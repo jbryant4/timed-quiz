6b142fe7bd6005ad5480d354e842d1ad2cd21f4e
JoKing <joebryant614@gmail.com>
2021-04-03 14:13:27 -0700

    base quiz structure

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,11 +5,7 @@ var quizQuestion = [{ question: "Q1?", a: "answer1", b: "answer2", c: "answer3",
 { question: "Q2?", a: "answer1", b: "answer2", c: "answer3", d: "answer4", correct: "b" },
 { question: "Q3?", a: "answer1", b: "answer2", c: "answer3", d: "answer4", correct: "a" },
 { question: "Q4?", a: "answer1", b: "answer2", c: "answer3", d: "answer4", correct: "c" }];
-
-// console.log(questionEl[0]);
-// console.log(questionEl[1]);
-// console.log(questionEl[2]);
-// console.log(questionEl[3]);
+var currentQuestion = 0;
 
 // ? Create question container and apply style to the elements 
 
@@ -22,13 +18,26 @@ var questionEl = document.createElement("h2");
 // create ul to store li
 var answerList = document.createElement("div");
 answerList.setAttribute("id", "button-list");
-console.log(answerList);
 // TODO give style to dive 
-// create list of buttons for each answer option
+// create list of buttons for each answer option and give class to all 
 var answer1 = document.createElement("button");
+answer1.className = "answer-button"
+answer1.setAttribute("data-answer", "a")
+
 var answer2 = document.createElement("button");
+answer2.className = "answer-button"
+answer2.setAttribute("data-answer", "b")
+
 var answer3 = document.createElement("button");
+answer3.className = "answer-button"
+answer3.setAttribute("data-answer", "c")
+
 var answer4 = document.createElement("button");
+answer4.className = "answer-button"
+answer4.setAttribute("data-answer", "d")
+
+
+
 // TODO add style to question buttons also add types
 // create space for answer return
 var resultArea = document.createElement("h3");
@@ -49,19 +58,40 @@ questionContainer.appendChild(resultArea);
 
 
 
+var rightAnswer = function () {
+    var userAnswer = this.getAttribute("data-answer")
+
+    if (userAnswer === quizQuestion[currentQuestion].correct) {
+        alert("yes");
+
+    } else {
+        alert("no");
+    }
+    
+    if (currentQuestion === quizQuestion.length - 1) {
+        alert("end of quiz")
+
+    } else {
+        currentQuestion++;
+        promptQuestions();
+    }
+}
+
 var promptQuestions = function () {
 
-    questionEl.textContent = quizQuestion[0].question;
-    answer1.textContent = quizQuestion[0].a;
-    answer2.textContent = quizQuestion[0].b;
-    answer3.textContent = quizQuestion[0].c;
-    answer4.textContent = quizQuestion[0].d;
+    questionEl.textContent = quizQuestion[currentQuestion].question;
+    answer1.textContent = quizQuestion[currentQuestion].a;
+    answer2.textContent = quizQuestion[currentQuestion].b;
+    answer3.textContent = quizQuestion[currentQuestion].c;
+    answer4.textContent = quizQuestion[currentQuestion].d;
 
     quizArea.appendChild(questionContainer);
 
+    document.querySelectorAll(".answer-button").forEach((Element) => {
+        Element.addEventListener("click", rightAnswer);
+    });
 
 
 }
-
 // event starters 
 quizStart.addEventListener("click", promptQuestions);
