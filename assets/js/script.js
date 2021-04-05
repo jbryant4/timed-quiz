b8f59677b448223e14a566dc1af6333415e433cf
JoKing <joebryant614@gmail.com>
2021-04-05 15:08:38 -0700

    timer issues

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-// global variables
+// ? GLOBAL VARIABLES
 var quizArea = document.querySelector("#quiz-area")
 var quizStart = document.querySelector("#quiz-start")
 var quizQuestion = [{ question: "Q1?", a: "answer1", b: "answer2", c: "answer3", d: "answer4", correct: "a" },
@@ -6,9 +6,11 @@ var quizQuestion = [{ question: "Q1?", a: "answer1", b: "answer2", c: "answer3",
 { question: "Q3?", a: "answer1", b: "answer2", c: "answer3", d: "answer4", correct: "a" },
 { question: "Q4?", a: "answer1", b: "answer2", c: "answer3", d: "answer4", correct: "c" }];
 var currentQuestion = 0;
-var quizTimerLength = 90;
-// ? Create question container and apply style to the elements 
+var quizTimerLength = 35;
 
+
+
+// ? Create question container and apply style to the elements 
 // create question container
 var questionContainer = document.createElement("div");
 // TODO give style to container
@@ -36,9 +38,6 @@ var answer4 = document.createElement("button");
 answer4.className = "answer-button";
 answer4.setAttribute("data-answer", "d");
 // TODO add style to question buttons also add types
-// create space for answer return
-var resultArea = document.createElement("h3");
-// TODO add style
 
 
 // In this order create question area 
@@ -53,36 +52,89 @@ questionContainer.appendChild(questionEl);
 questionContainer.appendChild(answerList);
 // result in container
 questionContainer.appendChild(resultArea);
+// ? END OF QUESTION CONTAINER CREATION
 
 
-var quizTimer = function () {
-    setInterval(function () {
+// ? TIMER FUNCTION
+var timer;
+function quizTimer() {
+    timer = setInterval(function () {
         quizTimerLength--;
         document.querySelector("#timer-area").textContent = quizTimerLength;
+
+        if (quizTimerLength < 30) {
+            document.getElementById("timer-area").style.color = "red";
+        }
+
+        if (quizTimerLength < 20) {
+            document.getElementById("timer-area").style.fontSize = "30px";
+        }
+
+        if (quizTimerLength === 0) {
+            endGame();
+        }
+
     }, 1000);
+
+
 }
 
-var rightAnswer = function () {
+// ? END GAME FUNCTION
+function endGame() {
+    questionContainer.remove();
+    clearInterval(timer)
+    // create form elements to submit score and give style 
+    var scoreContainer = document.createElement("div");
+    var scoreTitle = document.createElement("h2");
+    scoreTitle.textContent = "Your score is "
+    var score = document.createElement("span");
+    score.textContent = quizTimerLength;
+    var scoreForm = document.createElement("form");
+    var userLabel = document.createElement("label");
+    userLabel.textContent = "Your Name"
+    var userName = document.createElement("input");
+    userName.setAttribute("type", "text");
+    userName.setAttribute("id", "user-name")
+    var saveButton = document.createElement("input");
+    saveButton.setAttribute("type", "submit")
+    saveButton.setAttribute("id", "savescore-btn")
+    // prompt score div 
+    scoreForm.appendChild(userLabel);
+    scoreForm.appendChild(userName);
+    scoreForm.appendChild(saveButton);
+    scoreTitle.appendChild(score);
+    scoreContainer.appendChild(scoreTitle);
+    scoreContainer.appendChild(scoreForm);
+    // put on screen
+    quizArea.appendChild(scoreContainer);
+}
+
+// ? ANSWER FUNCTION
+function rightAnswer() {
+
     var userAnswer = this.getAttribute("data-answer")
 
     if (userAnswer === quizQuestion[currentQuestion].correct) {
-        alert("yes");
-
+        // add sound
     } else {
-        alert("no");
+        quizTimerLength = quizTimerLength - 10;
+        // add sound
     }
-
     if (currentQuestion === quizQuestion.length - 1) {
-        alert("end of quiz")
-
-    } else {
+        endGame();
+    }
+    else {
         currentQuestion++;
         promptQuestions();
     }
 }
 
-var promptQuestions = function () {
-    quizTimer()
+// ? PROMPT QUESTIONS FUNCTION
+function promptQuestions() {
+    quizStart.remove();
+    if (quizTimerLength === 35) {
+        quizTimer()
+    }
 
     questionEl.textContent = quizQuestion[currentQuestion].question;
     answer1.textContent = quizQuestion[currentQuestion].a;
