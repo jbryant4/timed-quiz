e4920802f8ad65bf0547bd21552dd2789716b11d
JoKing <joebryant614@gmail.com>
2021-04-07 00:42:07 -0700

    adding more style

--- assets/js/highscore.js
+++ assets/js/highscore.js
@@ -3,22 +3,27 @@ var scoreList = document.createElement("ul");
 
 // ? LOAD HIGHSCORES ON HIGHSCORE PAGE
 function loadHighScores() {
+    var checkList = localStorage.getItem("hsList");
+    if (checkList === null) {
+            console.log("empty");
+    } else {
+        var hsList = JSON.parse(checkList);
 
-    var hsList = JSON.parse(localStorage.getItem("hsList"));
-    console.log(hsList)
-    var savedHighScores = hsList.score;
-    var savedWhovian = hsList.whovian;
-    var scoreArea = document.getElementById("highscores");
 
-    var scoreListTitle = document.createElement("h2");
-    scoreListTitle.textContent = "HIGHSCORES";
-    
-    var scoreListItem = document.createElement("li");
-    scoreListItem.textContent = savedWhovian + savedHighScores;
+        var savedHighScores = hsList.score;
+        var savedWhovian = hsList.whovian;
+        var scoreArea = document.getElementById("highscores");
 
-    scoreList.appendChild(scoreListItem);
-    scoreArea.appendChild(scoreListTitle);
-    scoreArea.appendChild(scoreList);
+        var scoreListTitle = document.createElement("h2");
+        scoreListTitle.textContent = "HIGHSCORES";
+
+        var scoreListItem = document.createElement("li");
+        scoreListItem.textContent = savedWhovian + savedHighScores;
+
+        scoreList.appendChild(scoreListItem);
+        scoreArea.appendChild(scoreListTitle);
+        scoreArea.appendChild(scoreList);
+    }
 }
 
 
@@ -27,12 +32,12 @@ function loadHighScores() {
 var deleteScores = document.querySelector("#delete-scores");
 deleteScores.addEventListener("click", deleteHighScores);
 
-function deleteHighScores(){
+function deleteHighScores() {
     var confirm = window.confirm("Are you sure?");
 
     if (confirm) {
         scoreList.remove();
-    }else {
+    } else {
         location.reload();
     }
 }
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,22 @@
 // ? GLOBAL VARIABLES
 var quizArea = document.querySelector("#quiz-area")
 var quizStart = document.querySelector("#quiz-start")
-var quizQuestion = [{ question: "Q1?", a: "answer1", b: "answer2", c: "answer3", d: "answer4", correct: "a" },
-{ question: "Q2?", a: "answer1", b: "answer2", c: "answer3", d: "answer4", correct: "b" },
-{ question: "Q3?", a: "answer1", b: "answer2", c: "answer3", d: "answer4", correct: "a" },
-{ question: "Q4?", a: "answer1", b: "answer2", c: "answer3", d: "answer4", correct: "c" }];
-var currentQuestion = 0;
-var quizTimerLength = 35;
-var hsList = [];
 
+var quizQuestion = [{ question: "Q1: Who/What is Bad Wolf?", a: "A scary wolf like Alien", b: "Captain Jack Harkness", c: "Rose Tyler", d: "Donna Noble", correct: "c" },
+{ question: "Q2: Who/What is the Face of Boe?", a: "Donna Noble", b: "Rose Tyler", c: "Martha Jones", d: "Captain Jack Harkness", correct: "d" },
+{ question: "Q3: The 11th Doctor really likes an odd hat that no one else likes. What is it?", a: "Fedora", b: "Fezz", c: "Top Hat", d: "Deerstalker", correct: "b" },
+{ question: "Q4: The first time we meet the Impossible Girl she is trying to bake what?", a: "Soufflés", b: "Cookies", c: "Cupcakes", d: "Darlek Pie", correct: "a" },
+{ question: "Q5: Who waited for 1894 years and guarded the Pandorica?", a: "The Doctor", b: "A Cyberman", c: "Rory Williams", d: "Captain Jack Harkness", correct: "c" },
+{ question: "Q6: Who is the Doctor's oldest friend?", a: "K9", b: "The Master", c: "Harriet Jones", d: "Rose Tyler", correct: "b" },
+{ question: "Q7: What family is the in-laws to the Doctor?", a: "The Nobles", b: "The Jones", c: "The Ponds", d: "The Bryants", correct: "c" },
+{ question: "Q8: What makes River Song so special?", a: "She is an archaeologist", b: "She is married to a Time-Lord", c: "She killed the Doctor", d: "Her time line is reversed", correct: "d" },
+{ question: "Q9: The 12th Doctor makes a sonic out of?", a: "A guitar", b: "A Cane", c: "Sunglasses", d: "A Watch", correct: "c" },
+{ question: "Q10: The 11th doctor enjoys this food after his regeneration?", a: "Fish Sticks and Custard", b: "Bacon", c: "Apples and Peanut Butter", d: "Chips(aka fries)", correct: "a" }];
+
+var currentQuestion = 0;
+var quizTimerLength = 100;
+var hsList = [];
+var gameOver = false;
 
 
 // ? Create question container and apply style to the elements 
@@ -58,20 +66,19 @@ questionContainer.appendChild(answerList);
 var timer;
 function quizTimer() {
     timer = setInterval(function () {
-        quizTimerLength--;
-        document.querySelector("#timer-area").textContent = quizTimerLength;
-        if (quizTimerLength < 30) {
-            document.getElementById("timer-area").style.color = "red";
-        }
+        if (!gameOver) {
+            quizTimerLength--;
+            document.querySelector("#timer-area").textContent = quizTimerLength;
 
-        if (quizTimerLength < 20) {
-            document.getElementById("timer-area").style.fontSize = "30px";
-        }
+            if (quizTimerLength < 30) {
+                document.getElementById("timer-area").style.color = "red";
+            }
 
-        if (quizTimerLength === 0) {
-            endGame();
+            if (quizTimerLength === 0) {
+                gameOver = true;
+                endGame();
+            }
         }
-
     }, 1000);
 
 
@@ -110,13 +117,26 @@ function endGame() {
 }
 
 // ? SAVE SCORES TO LOCAL STORAGE
-function saveScores() {
+function saveScores(event) {
+    event.preventDefault();
+
+    var checkList = localStorage.getItem("hsList");
     var savedScores = quizTimerLength;
     var savedNames = document.getElementById("user-name").value;
-    var scoreObj = JSON.stringify({ whovian: savedNames, score: savedScores });
 
-    var hsList = localStorage.setItem("hsList", scoreObj);
+    if (checkList === null) {
 
+        var scoreObj = JSON.stringify([{ whovian: savedNames, score: savedScores }]);
+        var hsList = localStorage.setItem("hsList", scoreObj);
+
+    } else {
+
+        checkList = JSON.parse(checkList);
+        console.log(checkList)
+        checkList.push({ whovian: savedNames, score: savedScores });
+        localStorage.setItem("hsList", JSON.stringify(checkList));
+
+    }
 }
 
 
@@ -130,8 +150,11 @@ function rightAnswer() {
     } else {
         quizTimerLength = quizTimerLength - 10;
         // add sound
+        var darlek = new Audio('exterminate.mp3');
+        darlek.play();
     }
     if (currentQuestion === quizQuestion.length - 1) {
+        gameOver = true;
         endGame();
     }
     else {
@@ -143,7 +166,7 @@ function rightAnswer() {
 // ? PROMPT QUESTIONS FUNCTION
 function promptQuestions() {
     quizStart.remove();
-    if (quizTimerLength === 35) {
+    if (quizTimerLength === 100) {
         quizTimer()
     }
 
